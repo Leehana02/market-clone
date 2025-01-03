fa04a2fc3a50a2ab678a34757e7e2f466bb222fa
Leehana02 <hn110178@naver.com>
2025-01-03 10:04:07 +0900

    글쓰기. 로그인, 회원가입 페이지 만들기

---
+++ frontend/login.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("#login-form");
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const sha256Password = sha256(formData.get("password"));
+  formData.set("password", sha256Password);
+
+  const res = await fetch("/login", {
+    method: "POST",
+    body: formData,
+  });
+  const data = await res.json();
+  const accessToken = data.access_token;
+  window.localStorage.setItem("token", accessToken);
+  alert("로그인되었습니다.");
+
+  window.location.pathname = "/";
+
+  const infoDiv = document.querySelector("#info");
+  infoDiv.innerText = "로그인 성공";
+};
+
+form.addEventListener("submit", handleSubmit);
